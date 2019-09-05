fa74581b9622266826d5ab20390e57f2ab16175b
Jared Cabanilla <Jcabanilla@gmail.com>
2019-09-05 16:12:10 -0700

    updated index to show burgers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,35 +9,34 @@ var burger = require('../models/burger.js');
 
 //create our routes
 router.get("/", function (req, res) {
-    burger.selectAll(function (data) { 
+    burger.selectAll(function (data) {
         var burgerObject = {
             burgers: data
         };
         console.log(burgerObject);
         res.render("index", burgerObject);
-        
-    })
+    });
 });
 
 //create a burger
-router.post("/api/burgers", function(req, res) {
+router.post("/api/burgers", function (req, res) {
     burger.insertOne([
-      "burgerName", 'devoured'
+        "burgerName", 'devoured'
     ], [
-      req.body.burgerName, req.body.devoured
-    ], function(result) {
-      // Send back the ID of the new burger
-      res.json({ id: result.insertId });
+        req.body.burgerName, req.body.devoured
+    ], function (result) {
+        // Send back the ID of the new burger
+        res.json({ id: result.insertId });
     });
-  });
+});
 
-  // Set burger to be devoured.
-router.put("/api/burgers/:id", function(req, res) {
+// Set burger to be devoured.
+router.put("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
 
     console.log("condition", condition);
 
-    burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
+    burger.updateOne({ devoured: req.body.devoured }, condition, function (result) {
         if (result.changedRows === 0) {
             // If no rows were changed, then the ID must not exist, so 404.
             return res.status(404).end();
