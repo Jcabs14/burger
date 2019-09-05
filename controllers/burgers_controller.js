0c174a1a410f80cb48d260772ca2edf06bf1cdf6
Jared Cabanilla <Jcabanilla@gmail.com>
2019-09-05 15:13:59 -0700

    added pubilc folder and edited controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,17 +14,35 @@ router.get("/", function (req, res) {
             burgers: data
         };
         console.log(burgerObject);
+        res.render("index", burgerObject);
         
     })
 });
 
+//create a burger
 router.post("/api/burgers", function(req, res) {
     burger.insertOne([
-      "burgerName", 
+      "burgerName", 'devoured'
     ], [
-      req.body.burgerName,
+      req.body.burgerName, req.body.devoured
     ], function(result) {
       // Send back the ID of the new burger
       res.json({ id: result.insertId });
     });
   });
+
+  // Set burger to be devoured.
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
+        if (result.changedRows === 0) {
+            // If no rows were changed, then the ID must not exist, so 404.
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
